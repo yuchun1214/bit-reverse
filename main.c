e91eb0ffc346f6510f939c7a2a4a37d913da3cc1
Eugene Lin <lin.eugene.l.e@gmail.com>
2022-04-29 21:51:10 +0800

    First commit

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <math.h>
+
+
+// Teacher, I'am sorry because I haven't finished it
+// I spended a whole week on optimizing FFT
+// I will make up this as soon as possible.
+// Please don't failed me...
+int main(){
+    int p = 2;
+    int n = 8;
+
+    for(int i = 0; i < n; ++i){
+        
+    }
+}
