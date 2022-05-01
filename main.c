75abb15a87ab466f3b2a0e4993e90dd001b3fbb8
Eugene Lin <lin.eugene.l.e@gmail.com>
2022-05-01 13:55:54 +0800

    Finish basic feature of reverse bit

--- main.c
+++ main.c
@@ -1,15 +1,46 @@
 #include <math.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
 
+int main(int argc, const char *argv[]){
+    // Please change the code below, I use command line arguments as input
+    // k is the number you want to reverse
+    // p is the base which k is in.
+    //
+    // For example, if k = 15, p = 3
+    // The bits of k will be 120
+    // and the expected reverse of k is 012
+    int k = atoi(argv[1]);
+    int p = atoi(argv[2]);
 
-// Teacher, I'am sorry because I haven't finished it
-// I spended a whole week on optimizing FFT
-// I will make up this as soon as possible.
-// Please don't failed me...
-int main(){
-    int p = 2;
-    int n = 8;
+    int bit_array[1000];
 
-    for(int i = 0; i < n; ++i){
-        
+    memset(bit_array, 0, sizeof(bit_array));
+    
+    int count = 1;
+    int _k = k;
+    while((_k /= p) >= p) ++count; 
+    
+    _k = k;
+    double _pow;
+    for(int i = count+1; i >= 0; --i){
+        _pow = pow(p, i);
+        while((_k - _pow) >= 0){
+            _k -= _pow;
+            ++bit_array[i];
+        }
     }
+    printf("k = ");
+    for(int i = 0; i <= count; ++i){
+        printf("%d", bit_array[i]);
+    }
+    printf(" in base %d\n", p);
+    
+    printf("----------The reverse of k is---------- \n");
+    printf("j = ");
+    for(int i = count; i >= 0; --i){
+        printf("%d", bit_array[i]);
+    }
+    printf(" in base %d\n", p);
 }
